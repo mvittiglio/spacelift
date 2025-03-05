3b24cb684d2c88a5c20c75c77d0898a25fcde066
Michael Vittiglio <michael.vittiglio@gmail.com>
2025-03-05 12:32:42 -0500

    Updating AMI ID.

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 ### instance.tf ###
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-05b10e08d247fb927"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
   security_groups = [aws_security_group.allow_http.name]
