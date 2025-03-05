81c24ee2323b5b084a63dd1e47fcc182d1b9ead8
Michael Vittiglio <michael.vittiglio@gmail.com>
2025-03-05 17:27:56 -0500

    Update security.tf
    Moving to port 80.

--- security.tf
+++ security.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "allow_http" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 8800
+    from_port   = 80
     to_port     = 8800
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
