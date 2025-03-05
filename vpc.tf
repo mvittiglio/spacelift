0ac77fa59a80c99c5a14811df3e20a2ce4164f6c
Michael Vittiglio <michael.vittiglio@gmail.com>
2025-03-05 12:41:17 -0500

    Updating VPC to have an external gateway.

--- vpc.tf
+++ vpc.tf
@@ -9,3 +9,29 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
