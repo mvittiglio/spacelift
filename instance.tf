a45b797b85716a8f06566488a354e4e8740ab11c
Michael Vittiglio <michael.vittiglio@gmail.com>
2025-03-05 12:35:04 -0500

    More typos fixed.

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   ami           = "ami-05b10e08d247fb927"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.allow_http.name]
+  security_group_ids = [aws_security_group.allow_http.id]
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
