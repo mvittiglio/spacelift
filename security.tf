cc8058b136f706b57384e0416daa9bcb6972bb48
Michael Vittiglio <michael.vittiglio@gmail.com>
2025-03-06 19:25:28 -0500

    Update security.tf

--- security.tf
+++ security.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "allow_http" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8088
+    to_port     = 8088
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
